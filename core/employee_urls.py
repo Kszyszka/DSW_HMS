a7301c64cb01cacd94b981481f6a75e917efe250
Kszyszka <hager.krzysztof.hk@gmail.com>
2026-01-18 10:27:16 +0100

    Remove legacy URLs

--- core/employee_urls.py
+++ core/employee_urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('reservations/<int:pk>/', views.employee_reservation_detail, name='reservation_detail'),
     path('guests/', views.employee_guests, name='guests'),
     path('guests/<int:pk>/', views.employee_guest_detail, name='guest_detail'),
-    path('housekeeping/', views.employee_housekeeping, name='housekeeping'),
     path('maintenance/', views.employee_maintenance, name='maintenance'),
     path('pricing/', views.employee_pricing, name='pricing'),
     path('manager/employees/', views.manager_employees, name='manager_employees'),
